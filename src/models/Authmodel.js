5411da09254ae79b8a0bedc96e811c209b546c78
Sameer Sharma <202012102@daiict.ac.in>
2021-05-08 21:13:00 +0530

    minor change

--- src/models/Authmodel.js
+++ src/models/Authmodel.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose') //import
 const bodyparser = require('body-parser')
 const router = require('../../routes/auth')
 const dbUrl = process.env.DB_URL || 'mongodb://localhost/CodeMasters';
-mongoose.connect(dbUrl , {useNewUrlParser: true, useUnifiedTopology: true});
+//mongoose.connect(dbUrl , {useNewUrlParser: true, useUnifiedTopology: true});
+mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false
+  });
 const Schema = mongoose.Schema
 //creating the user schema
 const UserSchema = new Schema({
